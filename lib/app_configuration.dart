d3db567af0b3345dd953a91a71e94555ffcb52e7
Uladzimir Matveyeu <v.a.matveev96@gmail.com>
2022-12-09 15:02:04 +0100

    Move configuration related parameters to a separate file. (#15)
    Co-authored-by: Uladzimir Matveyeu <uladzimir_matveyeu@epam.com>

---
+++ lib/app_configuration.dart
@@ -0,0 +1,5 @@
+abstract class AppConfiguration {
+  static const String apiCulture = 'en';
+  static const String apiUrl = 'https://www.rijksmuseum.nl/api/$apiCulture';
+  static const String apiKey = '[api_key]';
+}
