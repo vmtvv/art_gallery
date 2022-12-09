863e95ed8a03c4938f9bce07a040db1962c5f541
Uladzimir Matveyeu <uladzimir_matveyeu@epam.com>
2022-12-09 15:01:33 +0100

    Move configuration related parameters to a separate file.

--- lib/api/api_service_builder.dart
+++ lib/api/api_service_builder.dart
@@ -1,21 +1,18 @@
 import 'package:art_gallery/api/api.dart';
+import 'package:art_gallery/app_configuration.dart';
 import 'package:chopper/chopper.dart';
 
 class ApiServiceBuilder {
-  static const String _culture = 'en';
-  static const String _baseUrl = 'https://www.rijksmuseum.nl/api/$_culture';
-  static const String _apiKey = '[api_key]';
-
   static ArtCollectionService createArtCollectionService() {
     return ArtCollectionService.create(
-      baseUrl: _baseUrl,
+      baseUrl: AppConfiguration.apiUrl,
       interceptors: {_addKeyInterceptor},
     );
   }
 
   static Request _addKeyInterceptor(Request request) {
     final parameters = Map.of(request.parameters)
-      ..putIfAbsent('key', () => _apiKey);
+      ..putIfAbsent('key', () => AppConfiguration.apiKey);
     return request.copyWith(parameters: parameters);
   }
 }
