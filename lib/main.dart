9fbac5aac9bd0ffb32d32ce9c003e2036a81a9f5
Uladzimir Matveyeu <uladzimir_matveyeu@epam.com>
2022-12-11 14:55:07 +0100

    Refactor App widget structure and move to a separate file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:art_gallery/api/api.dart';
+import 'package:art_gallery/app.dart';
 import 'package:art_gallery/domain/domain.dart';
-import 'package:art_gallery/gallery/gallery.dart';
 import 'package:art_gallery/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -25,44 +23,4 @@ void main() {
       child: const App(),
     ),
   );
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.appTitle),
-      ),
-      body: const GalleryPage(),
-    );
-  }
 }
