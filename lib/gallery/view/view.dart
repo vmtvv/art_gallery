f768ad08f6c88afcea3d83ae225d451ce753b1dd
Uladzimir Matveyeu <uladzimir_matveyeu@epam.com>
2022-12-22 12:23:30 +0100

    Remove incorrectly working grouping from the gallery page.

--- lib/gallery/view/view.dart
+++ lib/gallery/view/view.dart
@@ -1,5 +1,5 @@
 export 'gallery_error.dart';
-export 'gallery_item.dart';
+export 'gallery_list_item.dart';
 export 'gallery_list.dart';
 export 'gallery_page.dart';
 export 'gallery_section_header.dart';
