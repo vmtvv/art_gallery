1a67d24da675d9ef7b4f29fdb2403374aedac9ca
Uladzimir Matveyeu <v.a.matveev96@gmail.com>
2023-01-09 17:05:31 +0100

    Create reusable gallery filter chips item. (#35)
    Co-authored-by: Uladzimir Matveyeu <uladzimir_matveyeu@epam.com>

--- lib/gallery_filter/view/gallery_filter_chips_item.dart
+++ lib/gallery_filter/view/gallery_filter_chips_item.dart
@@ -1,27 +1,23 @@
-import 'package:art_gallery/gallery_filter/gallery_filter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class GalleryFilterChipsItem extends StatelessWidget {
-  const GalleryFilterChipsItem({Key? key}) : super(key: key);
+  const GalleryFilterChipsItem({
+    Key? key,
+    required this.title,
+    required this.onDeleted,
+  }) : super(key: key);
+
+  final String title;
+  final Function() onDeleted;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GalleryFilterBloc, GalleryFilterState>(
-        builder: (context, state) {
-      return CheckboxListTile(
-        title:
-            Text(AppLocalizations.of(context)!.gallery_filter_has_image_label),
-        value: state.hasImage,
-        onChanged: (bool? value) {
-          if (value != null) {
-            context
-                .read<GalleryFilterBloc>()
-                .add(GalleryFilterHasImageChanged(value));
-          }
-        },
-      );
-    });
+    return Container(
+      margin: const EdgeInsets.only(left: 8),
+      child: InputChip(
+        label: Text(title),
+        onDeleted: onDeleted,
+      ),
+    );
   }
 }
--- lib/gallery_filter/view/view.dart
+++ lib/gallery_filter/view/view.dart
@@ -1,4 +1,5 @@
 export 'gallery_filter_chips.dart';
+export 'gallery_filter_chips_item.dart';
 export 'gallery_filter_picker.dart';
 export 'involved_maker_input.dart';
 export 'has_image_selector.dart';
