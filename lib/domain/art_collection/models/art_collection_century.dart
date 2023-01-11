edaca5c615ca6e322af0f0f3d64c84372515701c
Uladzimir Matveyeu <uladzimir_matveyeu@epam.com>
2023-01-11 16:29:20 +0100

    Refactor century input values to reuse them.

--- lib/domain/art_collection/models/art_collection_century.dart
+++ lib/domain/art_collection/models/art_collection_century.dart
@@ -1,6 +1,30 @@
 class ArtCollectionCentury {
-  ArtCollectionCentury(this.value, this.name);
+  const ArtCollectionCentury(this.value, this.name);
 
   final int value;
   final String name;
 }
+
+const artCollectionCenturies = [
+  ArtCollectionCentury(1, 'I'),
+  ArtCollectionCentury(2, 'II'),
+  ArtCollectionCentury(3, 'III'),
+  ArtCollectionCentury(4, 'IV'),
+  ArtCollectionCentury(5, 'V'),
+  ArtCollectionCentury(6, 'VI'),
+  ArtCollectionCentury(7, 'VII'),
+  ArtCollectionCentury(8, 'VIII'),
+  ArtCollectionCentury(9, 'IX'),
+  ArtCollectionCentury(10, 'X'),
+  ArtCollectionCentury(11, 'XI'),
+  ArtCollectionCentury(12, 'XII'),
+  ArtCollectionCentury(13, 'XIII'),
+  ArtCollectionCentury(14, 'XIV'),
+  ArtCollectionCentury(15, 'XV'),
+  ArtCollectionCentury(16, 'XVI'),
+  ArtCollectionCentury(17, 'XVII'),
+  ArtCollectionCentury(18, 'XVIII'),
+  ArtCollectionCentury(19, 'XIX'),
+  ArtCollectionCentury(20, 'XX'),
+  ArtCollectionCentury(21, 'XXI'),
+];
--- lib/gallery_filter/view/century_input.dart
+++ lib/gallery_filter/view/century_input.dart
@@ -4,30 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-final _centuries = [
-  ArtCollectionCentury(1, 'I'),
-  ArtCollectionCentury(2, 'II'),
-  ArtCollectionCentury(3, 'III'),
-  ArtCollectionCentury(4, 'IV'),
-  ArtCollectionCentury(5, 'V'),
-  ArtCollectionCentury(6, 'VI'),
-  ArtCollectionCentury(7, 'VII'),
-  ArtCollectionCentury(8, 'VIII'),
-  ArtCollectionCentury(9, 'IX'),
-  ArtCollectionCentury(10, 'X'),
-  ArtCollectionCentury(11, 'XI'),
-  ArtCollectionCentury(12, 'XII'),
-  ArtCollectionCentury(13, 'XIII'),
-  ArtCollectionCentury(14, 'XIV'),
-  ArtCollectionCentury(15, 'XV'),
-  ArtCollectionCentury(16, 'XVI'),
-  ArtCollectionCentury(17, 'XVII'),
-  ArtCollectionCentury(18, 'XVIII'),
-  ArtCollectionCentury(19, 'XIX'),
-  ArtCollectionCentury(20, 'XX'),
-  ArtCollectionCentury(21, 'XXI'),
-];
-
 class CenturyInput extends StatefulWidget {
   const CenturyInput({Key? key}) : super(key: key);
 
@@ -66,10 +42,10 @@ class CenturyInputState extends State<CenturyInput> {
             child: DropdownButton<ArtCollectionCentury?>(
               value: state.century,
               items: [
-                for (int i = 0; i < _centuries.length; i++)
+                for (int i = 0; i < artCollectionCenturies.length; i++)
                   DropdownMenuItem(
-                    value: _centuries[i],
-                    child: Text(_centuries[i].name),
+                    value: artCollectionCenturies[i],
+                    child: Text(artCollectionCenturies[i].name),
                   ),
               ],
               onChanged: (item) => context
